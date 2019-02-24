05bc666b57866f72e5040ccd45b88afcecd74c6f
Daniel Arndt Alves <progdan@gmail.com>
2019-02-23 23:41:25 -0300

    Ajustes com o sensor de temperatura

--- MultiFuncShield/Termometro/Termometro.ino
+++ MultiFuncShield/Termometro/Termometro.ino
@@ -6,23 +6,22 @@
   Blog Eletrogate - http://blog.eletrogate.com/guia-completo-do-shield-multi-funcoes-para-arduino
 */
  
-#include <MultiFuncShield.h>                           // Biblioteca Multifunction shield
-#include <OneWire.h>                                   // Biblioteca OneWire
-#include <DallasTemperature.h>                         // Biblioteca do sensor DS18B20
+#include <TimerOne.h>
+#include <Wire.h>
+#include <MultiFuncShield.h>
  
- 
-#define ONE_WIRE_BUS A4                                // pino de dados DS18B20 = A4 do Arduino 
-OneWire oneWire(ONE_WIRE_BUS);                         // criando a instancia do sensor
-DallasTemperature sensors(&oneWire);
  
 void setup() {
-  sensors.begin();                                     // inicializando o sensor 
-  MFS.initialize(&Timer1);                             // inicializa a biblioteca Multi função
+  Timer1.initialize();
+  MFS.initialize(&Timer1);  // initialize multi-function shield library
+  
+  // Initialize using a low pass filter.
+  // Choose either: SMOOTHING_NONE, SMOOTHING_MODERATE or SMOOTHING_STRONG
+  MFS.initLM35(SMOOTHING_MODERATE);
 }
 
 void loop() {
-  sensors.requestTemperatures();                       // envia o camando para o sensor 
-  float tempCentigrade = sensors.getTempCByIndex(0);   // lê a temperatura no DS18B20 
-  MFS.write(tempCentigrade, 1);                        // mostra a temperatura no display(1 casa decimal) 
-  delay(200);                                          // atraso de 200 milisegundos 
+  int tempCentigrade = MFS.getLM35Data(); // get centigrade in 1/10 of degree.
+  MFS.write((float)tempCentigrade / 100, 1);  // display temp to 1 decimal place.
+  delay(100);
 }
