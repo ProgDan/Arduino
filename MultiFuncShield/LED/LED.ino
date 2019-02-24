8426a9a0521f122032bb21b75fc180fea8a69337
Daniel Arndt Alves <progdan@gmail.com>
2019-02-23 22:18:17 -0300

    LEDs do Shield Multi-função

---
+++ MultiFuncShield/LED/LED.ino
@@ -0,0 +1,33 @@
+/* 
+  Programa de teste dos LEDs do Shield Multi-função
+  Arduino UNO - Shield Multi-função para Arduino
+  Daniel Arndt Alves
+  Baseado em http://www.cohesivecomputing.co.uk/hackatronics/arduino-multi-function-shield/part-1/
+  Blog Eletrogate - http://blog.eletrogate.com/guia-completo-do-shield-multi-funcoes-para-arduino
+*/
+ 
+#include <TimerOne.h>                     // Bibliotec TimerOne 
+#include <Wire.h>                         // Biblioteca Wire 
+#include <MultiFuncShield.h>              // Biblioteca Multifunction shield
+ 
+void setup() {
+  Timer1.initialize();                    // inicializa o Timer 1
+  MFS.initialize(&Timer1);                // initializa a biblioteca Multi função
+}
+
+void loop() {
+  MFS.writeLeds(LED_ALL, ON);             // acende todos os LEDs
+  delay(1000);                            // atraso de 1 segundo
+  MFS.blinkLeds(LED_1, ON);               // pisca LED 1
+  delay(1000);                            // atraso de 1 segundo
+  MFS.blinkLeds(LED_2, ON);               // pisca LED 2
+  delay(1000);                            // atraso de 1 segundo
+  MFS.blinkLeds(LED_1 | LED_2, OFF);      // apaga LED 1 e 2
+  MFS.blinkLeds(LED_3 | LED_4, ON);       // pisca LED 3 e 4
+  delay(1000);                            // atraso de 1 segundo
+  MFS.blinkLeds(LED_ALL, ON);             // pisca todos os LEDs
+  delay(1000);                            // atraso de 1 segundo
+  MFS.blinkLeds(LED_ALL, OFF);            // apaga o pisca dos LEDs
+  MFS.writeLeds(LED_ALL, OFF);            // apaga o acender dos LEDs
+  delay(1000);                            // atraso de 1 segundo
+}
